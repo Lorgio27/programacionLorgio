212f38e91c69bfdcd6df78a72e657533f03183d5
Lorgio27 <arteagalorgio@gmail.com>
2026-09-21 12:40:11 +0200

    poco a poco

---
+++ test/src/ejercicio1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Ejercicio1");
+
+        Scanner teclado new Scanner(System.in);
+        System.out.printf()
+    }
+}
