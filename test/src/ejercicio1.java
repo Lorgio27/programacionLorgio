6b6395e93c3e8d1baa1f07d3b12a0f587eed9d27
Lorgio27 <arteagalorgio@gmail.com>
2026-09-23 08:53:03 +0200

    commit 23/09/206

--- test/src/ejercicio1.java
+++ test/src/ejercicio1.java
@@ -5,7 +5,17 @@ public class ejercicio1 {
         System.out.println("Ejercicio1");
 
         Scanner teclado new Scanner(System.in);
-        System.out.printf("ENTERO:")
-                
+        int numero1; 
+        int numero2;
+        int resultado;
+        int numero1 = teclado.nextInt(Valor);
+        int numero2 = teclado.nextInt(Valor);
+        System.out.println(resultado);
+        resultado = numero1 + numero2;
+
+
+
+
+
     }
 }
