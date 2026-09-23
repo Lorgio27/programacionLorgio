b255a914b51ca29f2a8f063a528dca9afcf069da
Lorgio27 <arteagalorgio@gmail.com>
2026-09-23 10:11:32 +0200

    ejercicio1 finish

--- test/src/ejercicio1.java
+++ test/src/ejercicio1.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 public class ejercicio1 {
     public static void main(String[] args) {
         System.out.println("Ejercicio1");
-
-        Scanner teclado new Scanner(System.in);
-        int numero1; 
+        Scanner teclado = new Scanner(System.in);
+        int numero1;
         int numero2;
         int resultado;
-        int numero1 = teclado.nextInt(Valor);
-        int numero2 = teclado.nextInt(Valor);
-        System.out.println(resultado);
+        double resultadoreal;
+        double resultadoreal2;
+        System.out.println("Introduce un numero");
+        numero1 = teclado.nextInt();
+        numero2 = teclado.nextInt();
         resultado = numero1 + numero2;
-
+        System.out.println(resultado);
+        resultado = numero1 - numero2;
+        System.out.println(resultado);
+        resultado = numero1 * numero2;
+        System.out.println(resultado);
+        resultadoreal = numero1 / numero2;
+        System.out.println(resultadoreal);
+        resultadoreal2= numero1 % numero2;
+        System.out.println(resultadoreal2);
 
 
 
